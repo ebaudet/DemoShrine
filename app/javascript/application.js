02875ef081992cd19da5f3c174f471da3ee0c5ef
Emilien Baudet <emilien.baudet@adevinta.com>
2025-07-22 21:11:17 +0200

    fix files upload

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,10 @@
 // Entry point for the build script in your package.json
 
-// import Rails from "@rails/ujs"
-// import Turbolinks from "turbolinks"
+import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "./channels"
-import Rails from "@rails/ujs"
 
 Rails.start()
-// Turbolinks.start()
 ActiveStorage.start()
 
 import 'bootstrap'
@@ -24,8 +21,7 @@ const imagePath = (name) => images(name, true)
 
 import { singleFileUpload, multipleFileUpload } from './fileUpload'
 
-// Use 'DOMContentLoaded' event if not using Turbolinks
-document.addEventListener('turbolinks:load', () => {
+document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('input[type=file]').forEach(fileInput => {
     if (fileInput.multiple) {
       multipleFileUpload(fileInput)
